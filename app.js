80367b0ee385ec81c03b186d29c44c31c6ee8ee5
Ravichandan Mahadev <Ravichandan Mahadev>
2016-09-23 18:23:32 -0700

    assignment

--- app.js
+++ app.js
@@ -3,8 +3,7 @@
 angular.module("ShoppingList",[])
 .controller("BuyController",BuyControllerFunction)
 .controller("BoughtController", BoughtControllerFunction)
-.provider("BuyService", BuyServiceProvider)
-.config(config);
+.service("BuyService", BuyService);
 
 BuyControllerFunction.$inject = ['BuyService'];
 function BuyControllerFunction(BuyService)
@@ -25,52 +24,27 @@ function BuyControllerFunction(BuyService)
     controller2.BoughtList = BuyService.GetBoughtList();
   }
 
-//BuyServiceProvider method
-function BuyServiceProvider()
-{
-  var provider = this;
-  //initialize blank array and masterlist will be populated in config service
-  provider.Default = {MasterList: []};
-  provider.$get = function(){
-    return new ShoppingService(provider.Default.MasterList);
-  }
-  return provider;
-}
-
- // shopping service with basic Get Buylist, BoughtList and Buy Item
-  function ShoppingService(list)
+ // buy service with basic Get Buylist, BoughtList and Buy Item
+  function BuyService()
   {
-    var service = this;
-    service.buylist =list;//initialize masterlist (From Configuration)
-    service.boughtlist = [];
-
-    service.GetBoughtList = function(){
+      var service = this;
+      service.buylist =[{item:"Cookie", quantity:4},
+                      {item:"Chips", quantity:12},
+                      {item:"Soda", quantity:18},
+                      {item:"Chocolates", quantity:20},
+                      {item:"Ice Cream", quantity:15},
+                      {item:"Donuts", quantity:10}];
+      service.boughtlist = [];
+      service.GetBoughtList = function(){
       return service.boughtlist;
-    }
-
-    service.GetBuyList = function(){
+      }
+      service.GetBuyList = function(){
       return service.buylist;
-    }
-
-    service.Buy= function(index){
-      var item = service.buylist[index];//Get the item from buy list
-      service.buylist.splice(index,1); //remove item from buylist
-      service.boughtlist.push(item); // add item to boughtlist
-    };
-
+      }
+      service.Buy= function(index){
+        var item = service.buylist[index];//Get the item from buy list
+        service.buylist.splice(index,1); //remove item from buylist
+        service.boughtlist.push(item); // add item to boughtlist
+      };
   }
-
-// Initialize the masterlist of buy items
-  function config(BuyServiceProvider)
-  {
-    //default buy list
-     BuyServiceProvider.Default.MasterList = [
-     {item:"Cookie", quantity:4},
-     {item:"Chips", quantity:12},
-     {item:"Soda", quantity:18},
-     {item:"Chocolates", quantity:20},
-     {item:"Ice Cream", quantity:15},
-     {item:"Donuts", quantity:10}];
-  }
-
 })();
